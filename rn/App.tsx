5da2bce128eae3ca0ac2defebb49ebe66d439139
Jobayerdev <jobayerhossain977@gmail.com>
2022-10-08 09:26:54 +0600

    main: Added native module

--- rn/App.tsx
+++ rn/App.tsx
@@ -1,15 +1,47 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {
+  Button,
+  NativeModules,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+} from 'react-native';
+import React, {useState} from 'react';
 
-import React from 'react';
-
+const {ReactOneCustomMethod} = NativeModules;
 const App = () => {
+  // our custom method
+
+  const [id, setId] = useState('Press the button to get The ID');
+
+  const getId = () => {
+    ReactOneCustomMethod.getPhoneID()
+      .then((res: string) => {
+        setId('ID: ' + res);
+      })
+      .catch((err: any) => {
+        console.error(err);
+      });
+  };
+
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <SafeAreaView style={styles.wrapper}>
+      <Text style={styles.id}>{id}</Text>
+      <Button title="Get Id" onPress={getId} />
+    </SafeAreaView>
   );
 };
 
-export default App;
+const styles = StyleSheet.create({
+  wrapper: {
+    display: 'flex',
+    flex: 1,
+    justifyContent: 'center',
+    paddingHorizontal: 20,
+  },
+  id: {
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+});
 
-const styles = StyleSheet.create({});
+export default App;
